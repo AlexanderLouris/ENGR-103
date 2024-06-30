3529cd71a5fd1ccb3dbcd8094f19115d33a85c79
AlexanderLouris <lourisa@oregonstate.edu>
2024-06-29 18:34:45 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Alexander Louris
+# GitHub username: AlexanderLouris
+# Date: 06/29/2024
+# Description: Asks the user for their favorite animal and then prints out that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
